8ab3dddae4a5b32a14a1f06aa24c412006093101
AniramOcean <marisergisdottir@gmail.com>
2020-08-08 16:25:10 +0300

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import {Environment} from './interface';
-
-export const environment: Environment = {
-  production: true,
-  apiKey: "AIzaSyC6SFuE0y5-WoQjUIvVJ-OsU4Gh2erVLsk",
-  fbDbUrl: 'https://angular-blog-29bf4.firebaseio.com/'
-};
